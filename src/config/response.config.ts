938dbe8844a49edbd93676cf4ca3ee219dc4eb71
PattyCutie <iamsureiamcute@gmail.com>
2023-08-21 14:04:03 +0200

    Init and setup base development branch

---
+++ src/config/response.config.ts
@@ -0,0 +1,41 @@
+export default {
+  SUCCESS: {
+    statusCode: 200,
+    message: 'Success',
+  },
+  INTERNAL_SERVER_ERROR: {
+    statusCode: 500,
+    message: 'Interval Server Error',
+  },
+  HTTP_BAD_REQUEST: {
+    statusCode: 400,
+    message: 'Http Bad Request',
+  },
+  UNAUTHORIZED: {
+    statusCode: 401,
+    message: 'Unatuhorized',
+  },
+  NOT_FOUND: {
+    statusCode: 404,
+    message: 'Not Found',
+  },
+  EXAM_NOT_FOUND: {
+    statusCode: 101,
+    message: 'Exam Not Found',
+  },
+  EXAM_SUBJECT_NOT_FOUND: {
+    statusCode: 102,
+  },
+  EXAM_LEVEL_NOT_FOUND: {
+    statusCode: 103,
+    message: 'Exam Level Not Found',
+  },
+  EXAM_SKILL_PART_NOT_FOUND: {
+    statusCode: 104,
+    message: 'Exam Skill Part Not Found',
+  },
+  EXAM_TOPIC_NOT_FOUND: {
+    statusCode: 105,
+    message: 'Exam Topic Not Found',
+  },
+};
